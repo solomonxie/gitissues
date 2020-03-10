40285334f64ee2f4c1689d33e6aa8cd710c1876e
Solomon Xie <solomonxiewise@gmail.com>
2020-03-10 22:58:17 +0800

    solomonxie updated on branch [master]

--- entry.sh
+++ entry.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
+chmod 700 /root/.ssh/id_rsa
 
 # start cron
-/usr/sbin/crond -fd 0 >> /tmp/cron.log 2>&1 &
+/usr/sbin/crond -fd 0 2>&1 > /tmp/cron.log &
 # /usr/sbin/crond -f -l 8
-
-# SSH
-chmod 700 /root/.ssh/id_rsa
