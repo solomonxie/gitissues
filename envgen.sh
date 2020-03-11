46a55e5d9693f1d7b22ac52155fcae59a5267364
Solomon Xie <solomonxiewise@gmail.com>
2020-03-11 21:14:37 +0800

    solomonxie updated on branch [master]

--- envgen.sh
+++ envgen.sh
@@ -2,6 +2,7 @@
 #                      LOCAL SECRETS GENERATION                       #
 #######################################################################
 
+echo > /tmp/env.txt
 echo GH_USER="$(git config --global user.name)" >> /tmp/env.txt
 echo GH_EMAIL="$(git config --global user.email)" >> /tmp/env.txt
 echo GH_TOKEN="$(cat ~/.ssh/github_token.txt)" >> /tmp/env.txt
